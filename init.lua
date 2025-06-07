cffd6fb7765b459c577fd094bee7fc4930d98e4a
Mantas Cepulkovskis <mantas.cepulkovskis@gmail.com>
2025-06-07 14:41:01 +0300

    Add debug which-key group

--- init.lua
+++ init.lua
@@ -350,6 +350,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>d', group = '[D]ebug' },
       },
     },
   },
