e9f3bf519016616ca060def2015e9eabc0ce2074
Mantas Cepulkovskis <mantas.cepulkovskis@gmail.com>
2025-06-07 14:35:50 +0300

    Enable indent line; Switch auto pairs plugin

--- init.lua
+++ init.lua
@@ -942,7 +942,6 @@ require('lazy').setup({
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
-      require('mini.pairs').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
@@ -981,9 +980,9 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
